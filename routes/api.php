ac09c68a28f0e27fdccbbc399903c1b0c50d4ce5
Khaled <khalegjr@gmail.com>
2022-12-10 16:41:41 -0300

    Cria as rotas para a feature produtos

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('clientes', ClientesController::class);
+Route::apiResource('produtos', ProdutosController::class);
